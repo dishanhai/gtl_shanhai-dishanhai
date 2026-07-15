24c4819196a6f4216a74c4033c565a1cc30de7ce
dishanhai <phonediyi@qq.com>
2026-07-16 01:13:37 +0800

    feat: 添加原初临界加工配方逻辑（任务 1/5）

---
+++ src/main/java/com/dishanhai/gt_shanhai/common/machine/primordial/module/processing/PrimordialCriticalProcessingModuleLogic.java
@@ -0,0 +1,11 @@
+package com.dishanhai.gt_shanhai.common.machine.primordial.module.processing;
+
+import com.dishanhai.gt_shanhai.common.machine.primordial.PrimordialModuleRecipeLogic;
+import com.gtladd.gtladditions.api.machine.wireless.GTLAddWirelessWorkableElectricMultipleRecipesMachine;
+
+public class PrimordialCriticalProcessingModuleLogic extends PrimordialModuleRecipeLogic {
+
+    public PrimordialCriticalProcessingModuleLogic(GTLAddWirelessWorkableElectricMultipleRecipesMachine machine) {
+        super(machine);
+    }
+}
